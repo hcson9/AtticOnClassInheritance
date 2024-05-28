87eeb00807092e597a11172b995fad6d7eb23aa8
hochan <hochan9@gmail.com>
2024-05-28 11:41:45 +0900

    feat: init project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * create on 2024/05/28 create by IntelliJ IDEA.
+ *
+ * <p> New Project. </p>
+ *
+ * @author HoChan Son (hcson)
+ * @version 1.0
+ * @since 1.0
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
